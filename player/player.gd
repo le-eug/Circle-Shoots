980f339c19d08aecc730369ea3307fa11d4153d2
eugsharp <eugenebriones7135@gmail.com>
2024-12-14 21:29:52 +0800

    basic player movement and shooting mechanics

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,32 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var gun = $Gun
+@onready var shoot_point = $Gun/ShootPoint
+@export var bullet_speed = 1000
 
-func get_input():
+func basic_movement():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	velocity = input_direction.normalized() * speed
+	
+func rotate_gun():
+	gun.look_at(get_global_mouse_position())
+
+func shoot_handler():
+	if Input.is_action_just_pressed("shoot"):
+		print("shoot")
+		
+		# instantiate the bullet object 
+		var bullet_obj = load("res://bullet/Bullet.tscn").instantiate()
+		add_sibling(bullet_obj)
+		bullet_obj.rotation = gun.rotation
+		bullet_obj.global_position = shoot_point.global_position
+		bullet_obj.apply_impulse(Vector2(bullet_speed,0).rotated(gun.rotation), shoot_point.global_position)
+		
+		# timer for cooldown
 
 func _physics_process(delta):
-	get_input()
+	basic_movement()
+	rotate_gun()
+	shoot_handler()
 	move_and_slide()
